c366779023445cdd00cc4deef9abfcba3d90a9ee
Rujuta <rphalak@castlighthealth.com>
2018-10-20 15:09:35 -0700

    Well formed brackets using recursion

--- Recursion/NonEmptySubsetSum/src/NonEmptySubsetSum.java
+++ Recursion/NonEmptySubsetSum/src/NonEmptySubsetSum.java
@@ -1,3 +1,100 @@
+/*
+Possible To Achieve Target Sum?
+
+
+
+
+
+Problem Statement:
+
+
+
+Given an array arr of size n and a target sum k.
+
+
+
+You have to determine, whether there exists a group of numbers (numbers need not to be contiguous and group can not be empty) in arr such that their sum equals to k.
+
+
+
+Input Format:
+
+
+
+There are two argument. First one is arr and second one is k.
+
+
+
+Output Format:
+
+
+
+Return a boolean denoting your answer.
+
+
+
+Constraints:
+
+
+
+1 <= n <= 18
+-10^17 <= arr[i], k <= 10^17
+
+
+Sample Test Cases:
+
+
+
+Sample Input 1:
+
+
+
+arr = [2 4 8]
+
+k = 6
+
+
+
+Sample Output 1:
+
+
+
+True
+
+
+
+Explanation 1:
+
+
+
+arr[0] + arr[1] = 6
+
+
+
+Sample Input 2:
+
+
+
+arr = [2 4 6]
+
+k = 5
+
+
+
+Sample Output 2:
+
+
+
+False
+
+
+
+Explanation 2:
+
+
+
+There does not exists any group such that its sum equals to k.
+ */
 public class NonEmptySubsetSum {
 
     public static void main(String []args){
@@ -13,9 +110,9 @@ public class NonEmptySubsetSum {
         return check_sum_rec(arr,0,k,false);
     }
 
-    /* We can keep a count at each subset, but that solution has a problem
-      when negative numbers come into picture because the sum can change
-      with negative numbers.
+    /* We can keep a total/difference(will need one int variable less than total) at each subset. To handle empty subsets
+       with target of 0, that should not return true, hence there is a flag to check in the recurring function that tells if the set
+       is empty or not.
     */
     private static boolean check_sum_rec(long[] arr, int i, long difference, boolean notEmpty){
 
